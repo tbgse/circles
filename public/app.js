0f403f545221e3b605fc987f65e82fbf5dcf72d1
Tobias Guse <tobe.guse@gmail.com>
2016-02-23 07:57:08 +0000

    added usernames and timestamps

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 var socket=io();
 $(document).ready(function(){
+        var username;
+
     $('#message').keydown(function(event){
         if (event.which === 13){
             $('#chat').submit();
@@ -7,20 +9,28 @@ $(document).ready(function(){
 
         }
     })
-    console.log(document.getElementById('chat'))
+    $('#setUsername').submit(function(event){
+        event.preventDefault();
+        username = $('#username').val();
+        $('.overlay').remove();
+        $('.overlay-message').remove();
+    })
     $('#chat').submit(function(event){
     event.preventDefault();
-    var username = $('#username').val();
     var message = $('#message').val();
+    var d = new Date();
+    var timestamp = d.getHours() + ':' + d.getMinutes();
     socket.emit('text message',{username:username,message:message});
-        $('#content').append('<div class="message-bubble-container"><div class="user-image right"></div><div class="message-bubble-right">'+$('#message').val()+'</div></div>')  
- 
-
+        $('#content').append('<div class="message-bubble-container"><div class="user-image right"></div><div class="message-bubble-right"><p class="username">Me:</p>'+$('#message').val()+'<p class="timestamp">'+timestamp+'</p></div></div>')  
+    console.log(document.getElementById('content').scrollHeight);
+    $('#content').scrollTop(document.getElementById('content').scrollHeight)
     $('#message').val('').focus();
     return false;
     })
     
     socket.on('text message',function(msg){
-    $('#content').append('</div><div class="message-bubble-container"><div class="user-image left"></div><div class="message-bubble-left">'+msg.message+'</div></div>')   
+    var d = new Date();
+    var timestamp = d.getHours() + ':' + d.getMinutes();
+    $('#content').append('</div><div class="message-bubble-container"><div class="user-image left"></div><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>')   
     })
 })
