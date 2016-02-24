3cde3a7a06499120d3df60820e76db0304d54c15
Tobias Guse <tobe.guse@gmail.com>
2016-02-24 15:54:06 +0000

    user images

--- public/app.js
+++ public/app.js
@@ -1,7 +1,6 @@
 var socket=io();
 $(document).ready(function(){
         var username;
-
     $('#message').keydown(function(event){
         if (event.which === 13){
             $('#chat').submit();
@@ -22,8 +21,7 @@ $(document).ready(function(){
     var d = new Date();
     var timestamp = d.getHours() + ':' + d.getMinutes();
     socket.emit('text message',{username:username,message:message});
-        $('#content').append('<div class="message-bubble-container"><div class="user-image right"></div><div class="message-bubble-right"><p class="username">Me:</p>'+$('#message').val()+'<p class="timestamp">'+timestamp+'</p></div></div>')  
-    console.log(document.getElementById('content').scrollHeight);
+    $('#content').append('<div class="message-bubble-container"><img class="user-image right" src="icons/'+username+'.svg"><div class="message-bubble-right"><p class="username">Me:</p>'+$('#message').val()+'<p class="timestamp">'+timestamp+'</p></div></div>')  
     $('#content').scrollTop(document.getElementById('content').scrollHeight)
     $('#message').val('').focus();
     return false;
@@ -32,11 +30,27 @@ $(document).ready(function(){
     socket.on('text message',function(msg){
         var d = new Date();
         var timestamp = d.getHours() + ':' + d.getMinutes();
-        $('#content').append('<div class="message-bubble-container"><div class="user-image left"></div><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>')   
+        $('#content').append('<div class="message-bubble-container"><img class="user-image left" src="icons/'+msg.username+'.svg"><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>') 
+        $('#content').scrollTop(document.getElementById('content').scrollHeight)
+    })
+    socket.on('channel load',function(users){
+            $('#content').append('<div class="info-message">'+users[users.length-1]+' joined the chat.</div>')
+            $('#content').scrollTop(document.getElementById('content').scrollHeight)
+            $('#users').empty();
+        console.log(users)
+         users.forEach(function(x){
+             $('#users').append('<div class="user-container" id='+x+'><img class="user-image small" src="icons/'+x+'.svg"><span>'+x+'</span></div>')
+         })  
+
     })
     socket.on('channel join',function(user){
-        console.log(user)
-         $('#content').append('<div class="info-message">'+user[user.length-1]+' joined the chat.</div>')
-         $('#users').html(user)
+            $('#users').append('<div class="user-container" id='+user+'><img class="user-image small" src="icons/'+user+'.svg"><span>'+user+'</span></div>')
+    })
+    socket.on('channel leave',function(username){
+        $('#content').append('<div class="info-message">'+username+' left the chat.</div>')
+        $('#content').scrollTop(document.getElementById('content').scrollHeight)
+        $('#'+username).fadeOut(500,function(){
+            $(this).remove();
+        });
     })
 })
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express'),
     http = require('http'),
     router = express(),
     server = http.createServer(router),
+    jdenticon = require('jdenticon'),
+    fs = require('fs'),
+    md5 = require('js-md5')
     io = require('socket.io')(server);
 
 router.get('/',function(req,res){
@@ -10,16 +13,31 @@ router.get('/',function(req,res){
 
 router.use(express.static('public'))
 var activeUsers = [];
+console.log("test123")
 io.on('connection',function(socket){
-    
+    var username;
+    console.log(activeUsers)
     console.log(socket.id)
     console.log('a user connected');
     socket.on('disconnect',function(){
-         
+        console.log(socket.id + ' is leaving the chat')
+        console.log("it's the user "+username)
+        activeUsers.splice(activeUsers.indexOf(username),1);
+        io.emit('channel leave',username)
+        fs.unlink(process.cwd()+"/public/icons/"+username+".svg")
     })
     socket.on('channel join',function(user){
         activeUsers.push(user)
-        io.emit('channel join',activeUsers);
+        username = user;
+        var hash = md5(username);
+        var size = 50;
+        var svg = jdenticon.toSvg(hash, size);
+        socket.emit('channel load',activeUsers);
+        fs.writeFile(process.cwd()+"/public/icons/"+username+".svg",svg,function(err){
+            if (err) console.log(err);
+        socket.broadcast.emit('channel join',user);
+        })
+       
     })
     socket.on('text message',function(msg){
         socket.broadcast.emit('text message',msg)
@@ -31,3 +49,15 @@ io.on('connection',function(socket){
 server.listen(process.env.PORT,process.env.IP,function(){
     console.log('server listening on '+process.env.IP+' at port '+process.env.PORT)
 })
+
+
+String.prototype.hashCode = function() {
+  var hash = 0, i, chr, len;
+  if (this.length === 0) return hash;
+  for (i = 0, len = this.length; i < len; i++) {
+    chr   = this.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
