a3c85760f7f216c237d3c6499b792898b127a06f
Akira Laine <akira_laine@outlook.com>
2016-02-26 19:53:42 +1100

    added speech synthesis

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 var socket=io();
 $(document).ready(function(){
-        var username;
+    var username;
+    var voice = false;
     $('#message').keydown(function(event){
         if (event.which === 13){
             $('#chat').submit();
@@ -16,6 +17,15 @@ $(document).ready(function(){
         $('.overlay').remove();
         $('.overlay-message').remove();
     })
+    $("#voice").on("click", function(event){
+        if(voice === false){
+            voice = true;
+            $("#voice").text("Deactivate The Voice");
+        } else {
+            voice = false;
+            $("#voice").text("Activate The Voice");
+        }
+    })
     $('#chat').submit(function(event){
     event.preventDefault();
     var writing = $('#message').val();
@@ -25,22 +35,31 @@ $(document).ready(function(){
     var timestamp = d.getHours() + ':' + d.getMinutes();
     socket.emit('text message',{username:username,message:writing});
     var element = $('<div class="message-bubble-container">').append($('<img class="user-image right" src="icons/'+encodeFilename(username)+'.svg">')).append($('<div class="message-bubble-right">').append($('<p class="username">Me:</p>')).append(message).append($('<p class="timestamp">'+timestamp+'</p>')))
-    //$('#content').append('<div class="message-bubble-container"><img class="user-image right" src="icons/'+username+'.svg"><div class="message-bubble-right"><p class="username">Me:</p>'+message+'<p class="timestamp">'+timestamp+'</p></div></div>')  
+    //$('#content').append('<div class="message-bubble-container"><img class="user-image right" src="icons/'+username+'.svg"><div class="message-bubble-right"><p class="username">Me:</p>'+message+'<p class="timestamp">'+timestamp+'</p></div></div>')
+    var element = $('<div class="message-bubble-container">').append($('<img class="user-image right" src="icons/'+username+'.svg">')).append($('<div class="message-bubble-right">').append($('<p class="username">Me:</p>')).append(message).append($('<p class="timestamp">'+timestamp+'</p>')))
+    //$('#content').append('<div class="message-bubble-container"><img class="user-image right" src="icons/'+username+'.svg"><div class="message-bubble-right"><p class="username">Me:</p>'+message+'<p class="timestamp">'+timestamp+'</p></div></div>')
     $("#content").append(element)
     $('#content').scrollTop(document.getElementById('content').scrollHeight)
     $('#message').val('').focus();
     return false;
     })
-    
+
     socket.on('text message',function(msg){
+        console.log("THIS: " + msg)
         var d = new Date();
         var timestamp = d.getHours() + ':' + d.getMinutes();
         var message = $('<span />',{text:msg.message})
         console.log(message)
         var element = $('<div class="message-bubble-container">').append($('<img class="user-image left" src="icons/'+encodeFilename(msg.username)+'.svg">')).append($('<div class="message-bubble-left">').append($('<p class="username">'+msg.username+'</p>')).append(message).append($('<p class="timestamp">'+timestamp+'</p>')))
-        //$('#content').append('<div class="message-bubble-container"><img class="user-image left" src="icons/'+msg.username+'.svg"><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>') 
+        //$('#content').append('<div class="message-bubble-container"><img class="user-image left" src="icons/'+msg.username+'.svg"><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>')
+        var element = $('<div class="message-bubble-container">').append($('<img class="user-image left" src="icons/'+msg.username+'.svg">')).append($('<div class="message-bubble-left">').append($('<p class="username">'+msg.username+'</p>')).append(message).append($('<p class="timestamp">'+timestamp+'</p>')))
+        //$('#content').append('<div class="message-bubble-container"><img class="user-image left" src="icons/'+msg.username+'.svg"><div class="message-bubble-left"><p class="username">'+msg.username+':</p>'+msg.message+'<p class="timestamp">'+timestamp+'</div></div>')
         $('#content').append(element);
         $('#content').scrollTop(document.getElementById('content').scrollHeight)
+        if(voice){
+            var msg = new SpeechSynthesisUtterance(msg.message);
+            window.speechSynthesis.speak(msg);
+        }
     })
     socket.on('channel load',function(users){
         username = users[users.length-1];
@@ -50,7 +69,9 @@ $(document).ready(function(){
         console.log(users)
          users.forEach(function(x){
              $('#users').append('<div class="user-container" id='+x+'><img class="user-image small" src="icons/'+encodeFilename(x)+'.svg"><span>'+x+'</span></div>')
-         })  
+         })
+             $('#users').append('<div class="user-container" id='+x+'><img class="user-image small" src="icons/'+x+'.svg"><span>'+x+'</span></div>')
+         })
 
     })
     socket.on('channel join',function(user){
